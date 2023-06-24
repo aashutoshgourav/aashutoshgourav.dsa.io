76b7b956c42cee1376850c4f6584c21c5ea7e26c
aashutoshgourav <95736168+aashutoshgourav@users.noreply.github.com>
2023-06-24 10:07:59 +0530

    Update Assignment1_Fifth_Answer.java

--- Assignment 1/Assignment1_Fifth_Answer.java
+++ Assignment 1/Assignment1_Fifth_Answer.java
@@ -31,14 +31,11 @@ class MergeSortedArrays {
         MergeSortedArrays solution = new MergeSortedArrays();
 
         // Example usage
-        int[] nums1 = new int[6];
-        nums1[0] = 1;
-        nums1[1] = 3;
-        nums1[2] = 5;
+        int[] nums1 = {1,2,3,0,0,0};
         int m = 3;
 
-        int[] nums2 = { 2, 4 };
-        int n = 2;
+        int[] nums2 = { 2, 5,6 };
+        int n = 3;
 
         solution.mergeArrays(nums1, m, nums2, n);
 
